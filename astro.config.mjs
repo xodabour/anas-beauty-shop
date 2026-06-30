06f2fd2a1cd5745e89b9313bfb2263fa5ae5d626
xodabour <kikomellox1@gmail.com>
2026-06-30 16:09:15 -0300

    Sitemap automatico, robots.txt, metas navegacao interna,

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
-import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.anasbeautyshop.com.br",
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [
+    react(),
+    sitemap(),
+  ],
 });
